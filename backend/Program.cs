c7ac379c0914003e6d585e87b3f472e322de0830
Roberto Germiniani <rogermi67@gmail.com>
2026-01-01 19:33:46 +0100

    Remove duplicate CORS middleware

--- backend/Program.cs
+++ backend/Program.cs
@@ -115,29 +115,7 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Middleware CORS
-app.Use(async (context, next) =>
-{
-    var origin = context.Request.Headers["Origin"].ToString();
-    
-     if (!string.IsNullOrEmpty(origin))
-    {
-        context.Response.OnStarting(() => {
-            context.Response.Headers.Add("Access-Control-Allow-Origin", origin);
-            context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-            context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization");
-            context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-            return Task.CompletedTask;
-        });
-    }
-    
-    if (context.Request.Method == "OPTIONS")
-    {
-        context.Response.StatusCode = 204;
-        return;
-    }
-    
-    await next();
-});
+
 
 app.UseSwagger();
 app.UseSwaggerUI();
