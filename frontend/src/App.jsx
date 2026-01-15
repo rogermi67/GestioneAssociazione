d13db6a27dd2b554c6a382bad4eb32c6389e01a1
Roberto Germiniani <rogermi@gmail.com>
2026-01-15 22:12:31 +0100

    Add Eventi import in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-﻿import { Routes, Route, Navigate } from 'react-router-dom'
+﻿/* IMPORT */
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useAuthStore } from './stores/authStore'
